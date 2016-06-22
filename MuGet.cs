2859e182397f2a60fe1b75cac15dffcc19fbe84d
NiklasGit <niklas.kaulitz@gmail.com>
2016-06-22 14:30:23 +0000

    Add files via upload
    MuGet is the widget base class (Music Gadget... I know.. crative^^). Derive from it to implement your own widgets.
    MuGetSlider is an example widget I implemented quickly to give you guys an idea how it should look like.
    HandEventArgs is the event argument type which will be given by the application. All needed data of the hand event should be retrievable from there. If you need anything more just send me a massage.

---
+++ MuGet.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    //public delegate void HandEventHandler (HandEventArgs e);
+    class MuGet : Control
+    {
+        //default colors and stuff
+        private static Pen borderPen = System.Drawing.Pens.Black;
+        private static Color backgroundColor = Color.LightGray;
+        private static Color hoverColor = Color.DarkGray;
+        private static Pen hoverPen = System.Drawing.Pens.White;
+
+        private Pen currentPen;
+
+        //Constructor calls base class constructor of Control
+        public MuGet(String text, Int32 x, Int32 y, Int32 width, Int32 height)
+            : base(text, x, y, width, height)
+        {
+            currentPen = borderPen;
+            this.BackColor = backgroundColor;
+        }
+
+        /*Override this to draw stuff.
+         * Graphics g = e.CreateGraphics();
+         * Then draw with g. E.g.
+         *    System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(10, 10, 100, 100);
+         *    graphics.DrawRectangle(System.Drawing.Pens.Red, rectangle);
+        */
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //this.BackColor is used to draw the background. Because of this it is not necessary to draw a rectangle for the background
+            //draws a border
+            Graphics g = e.Graphics;
+            g.DrawRectangle(currentPen, 0, 0, this.Width - borderPen.Width, this.Height - borderPen.Width);
+            
+            base.OnPaint(e);
+        }
+
+        //Events
+        //Override to handle event
+        public virtual void HandStateChanged(HandEventArgs e)
+        {
+
+        }
+
+        public virtual void HandMoved(HandEventArgs e)
+        {
+
+        }
+
+        public virtual void HandEntered(HandEventArgs e)
+        {
+            this.BackColor = hoverColor;
+            this.currentPen = hoverPen;
+        }
+
+        //creates the hover effect for the widget
+        public virtual void HandLeft(HandEventArgs e)
+        {
+            this.BackColor = backgroundColor;
+            this.currentPen = borderPen;
+        }
+
+        public virtual void HandDragged(HandEventArgs e)
+        {
+
+        }
+
+        public virtual void HandClicked(HandEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ MuGetSlider.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    /*
+     * Sample MuGet implementation.
+     */
+    class MuGetSlider : MuGet
+    {
+        //default colors and stuff
+        //Pens are used for not filled objects.
+        private static Pen linePen = System.Drawing.Pens.Black;
+        //Brushes are used to fill objects.
+        private static Brush sliderBrush = System.Drawing.Brushes.Red;
+        private static Int32 sliderWidth = 10;
+        private static Int32 sliderHeight = 20;
+        private static Int32 sliderLineOffset = 10;
+
+        private Rectangle slider;
+        private bool dragMode = false;
+        HandEventArgs.HandSide dragSide;
+
+        //Constructor calls base class constructor of Control
+        public MuGetSlider(String text, Int32 x, Int32 y, Int32 width, Int32 height)
+            : base(text, x, y, width, height)
+        {
+            //creates the movable rectangle for the slider
+            slider = new System.Drawing.Rectangle(10, (int)(this.Height/2-sliderHeight/2), sliderWidth, sliderHeight);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //draws the line of the slider and the slider
+            Graphics g = e.Graphics;
+            g.DrawLine(linePen, new Point(sliderLineOffset, (int)this.Height/2), new Point(this.Width-sliderLineOffset, this.Height/2));
+            g.FillRectangle(sliderBrush, slider);
+
+            //calls the OnPaint of the base class. Without this the border would disappear.
+            base.OnPaint(e);
+        }
+
+        public override void HandStateChanged(HandEventArgs e)
+        {
+            //checks for the new hand state
+            if (e.getHandState() == HandEventArgs.HandState.Closed)
+            {
+                //checks if the hand position is within the slider. The coordinates are relative to the widget coordinates (not application).
+                if (slider.Contains(new Point(e.getHandX(), e.getHandY())))
+                {
+                    dragSide = e.agent;
+                    dragMode = true;
+                }
+            }
+            else
+            {
+                if (e.agent == dragSide)
+                {
+                    dragMode = false;
+                }
+            }
+        }
+
+        public override void HandMoved(HandEventArgs e)
+        {
+            //checks if in drag mode
+            if(dragMode)
+            {
+                slider.X = e.getHandX() - sliderWidth/2;
+
+                if (slider.X < 10) { slider.X = 10; }
+                if (slider.X > this.Width - sliderLineOffset - sliderWidth) { slider.X = this.Width - sliderLineOffset - sliderWidth; }
+            }
+            //repaints the widget. Without this the changes would not appear instantly.
+            this.Refresh();
+        }
+    }
+}
