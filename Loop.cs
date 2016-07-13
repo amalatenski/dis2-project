d8183a0fa9af7a0039aed6e860d164110b3680e3
NiklasGit <niklas.kaulitz@gmail.com>
2016-07-13 21:11:28 +0200

    Add files via upload
    SoundEngine now usable to create Loops.
    The Application needs one SoundEngine with wich all widgets may interact. Widgets need to store a reference.
    
    The constructor is a mess (I am trying out stuff there.). But the loop methods should work.
    With startPlayback the microfone input will be mapped to the speakers which can be stopped with stopPlayback.

---
+++ Loop.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSCore;
+using CSCore;
+using CSCore.SoundIn;
+using CSCore.SoundOut;
+using CSCore.Streams;
+using CSCore.Streams.Effects;
+using CSCore.XAudio2;
+using CSCore.DMO;
+using CSCore.Codecs.WAV;
+using CSCore.Codecs;
+
+namespace Test
+{
+    class Loop
+    {
+        static int count = 0;
+        public int id { get; private set; }
+        private string filename;
+
+        private WasapiCapture capture;
+        private ISoundOut soundOut;
+        private LoopStream loop;
+        private WaveWriter writer;
+
+        public Loop()
+        {
+            id = count;
+            count++;
+            filename = String.Format("{0}.wav", id);
+
+            soundOut = new WasapiOut();
+
+            init();
+        }
+
+        private void init()
+        {
+
+            capture = new WasapiCapture();
+            capture.Initialize();
+            SoundInSource captureSource = new SoundInSource(capture);
+
+            System.IO.File.Delete(filename);
+            writer = new WaveWriter(filename, captureSource.WaveFormat);
+            captureSource.DataAvailable += (s, e) =>
+            {
+                writer.Write(e.Data, e.Offset, e.ByteCount);
+            };
+        }
+
+        public bool isRecording()
+        {
+            if (capture.RecordingState == RecordingState.Recording) return true;
+            return false;
+        }
+
+        public void record()
+        {
+            if(loop == null) capture.Start();
+            else
+            {
+                if(soundOut.PlaybackState == PlaybackState.Playing)
+                {
+                    soundOut.Stop();
+                    loop.Dispose();
+                }
+                init();
+                capture.Start();
+            }
+        }
+
+        public void stopRecord()
+        {
+            capture.Stop();
+            capture.Dispose();
+
+            ((IDisposable)writer).Dispose();
+
+            IWaveSource file = CodecFactory.Instance.GetCodec(filename);
+            loop = new LoopStream(file);
+            loop.EnableLoop = true;
+            soundOut.Initialize(loop);
+        }
+
+        public void play()
+        {
+            soundOut.Play();
+        }
+
+        public void pause()
+        {
+            soundOut.Pause();
+        }
+
+        public void stop()
+        {
+            soundOut.Stop();
+            loop.Dispose();
+            soundOut.Dispose();
+        }
+    }
+}
